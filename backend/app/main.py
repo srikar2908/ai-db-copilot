36044d1d836ee775809b26a606405ef7e533d62e
srikar2908 <srikar.kukkadapu666@gmail.com>
2026-05-16 19:06:41 +0530

    Dockerized production-ready architecture

--- backend/app/main.py
+++ backend/app/main.py
@@ -64,6 +64,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5173",
         "http://localhost:5173",
+         "http://localhost:3000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
