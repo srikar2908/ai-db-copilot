1fd1ba4250b0684cf604f0e2a101e8b79fa054ef
srikar2908 <srikar.kukkadapu666@gmail.com>
2026-05-16 21:35:11 +0530

    Updated frontend services/api file with backend link to enable cors configuration

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -9,7 +9,8 @@ import { getAuthToken, logout } from '../utils/auth'
 
 import type { SchemaResponse } from '../types/schema'
 
-const API_BASE_URL = 'http://127.0.0.1:8000'
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
